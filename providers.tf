cae76c8b86e6eb0bbcc34ba05a96e955c23dac18
Ruslan <yarema2003r@gmail.com>
2023-10-27 21:52:11 +0300

    backend added

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
 #   }
 
   backend "s3" {
-    bucket            = "trfm-ste-bkt-final"
+    bucket            = "trfm-ste-bkt-final-demo3"
     key               = "global/mystatefile/terraform.tfstate"
     region            = "eu-north-1"
     dynamodb_table    = "terraform-lock"
