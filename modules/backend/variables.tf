cf1352b09202fd867c62104c8d43df8749fe19c2
Ruslan <yarema2003r@gmail.com>
2023-10-27 19:28:55 +0300

    backend added

---
+++ modules/backend/variables.tf
@@ -0,0 +1,21 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be globally unique."
+  type        = string
+  default     = "trfm-ste-bkt-final"
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table."
+  type        = string
+  default     = "terraform-lock"
+}
+
+variable "region" {
+    description = "The name of the region where we're working."
+    type        = string
+    default     = "eu-north-1"
+}
+
+variable "vpc_id" {
+  type = string
+}
