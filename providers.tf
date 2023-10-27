e3f225979c7c2d5047c1b23b805f4ba4d6a72025
Ruslan <yarema2003r@gmail.com>
2023-10-28 00:48:39 +0300

    backend added

--- providers.tf
+++ providers.tf
@@ -1,22 +1,22 @@
-terraform {
-#     cloud {
-# 	organization = "demo3_org"
-# 	workspaces {
-#   	name = "demo-workspace"
-# 	}
+# terraform {
+# #     cloud {
+# # 	organization = "demo3_org"
+# # 	workspaces {
+# #   	name = "demo-workspace"
+# # 	}
+# #   }
+
+  
+  
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.67.0"
+#     }
 #   }
 
-  
-  
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.67.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
+#   required_version = ">= 1.2.0"
+# }
 
 provider "aws" {
   region = "eu-north-1"
