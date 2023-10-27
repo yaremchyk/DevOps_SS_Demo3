62056f5136dd087b74f034c265e0e65a436f453f
Ruslan <yarema2003r@gmail.com>
2023-10-28 01:25:46 +0300

    backend added

--- providers.tf
+++ providers.tf
@@ -21,3 +21,8 @@ terraform {
 provider "aws" {
   region = "eu-north-1"
 }
+
+credentials "app.terraform.io" {
+  token = "HFBdwU076gXgKg.atlasv1.AggH0D35AuOyjcOAG55O11CX8ebZJstLIcsRKqviS9rGd1EocQfzfuCS3tsgmPPrqAQ"
+  # this being a team or user token (not an organisation token)
+}
