beacb5ac76ea49aea7aacfe1478be5adf967d062
Ruslan <yarema2003r@gmail.com>
2023-10-28 20:16:04 +0300

    ecs fixed

--- modules/ecs/launch_teamplate/main.tf
+++ modules/ecs/launch_teamplate/main.tf
@@ -23,7 +23,7 @@ resource "aws_launch_template" "ecs_launch_template" {
   name          = "${var.namespace}-ecs-launch-template-${var.environment}"
   image_id      = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
-  # key_name               = data.aws_key_pair.ssh.id
+  key_name               = data.aws_key_pair.ssh.id
   user_data              = base64encode(data.template_file.user_data.rendered)
   vpc_security_group_ids = [var.security_group_ec2_id]
 
