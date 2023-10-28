ecf30e7e78613639c2093d4d1b2f18cdf937a3ce
Ruslan <yarema2003r@gmail.com>
2023-10-29 00:22:01 +0300

    providers.tf fixed

--- providers.tf
+++ providers.tf
@@ -6,31 +6,23 @@ terraform {
       version = "4.67.0"
     }
   }
-      cloud {
- 	organization = "demo3_org"
-	workspaces {
-  	name = "demo-workspace"
+    cloud {
+ 	    organization = "demo3_org"
+        workspaces {
+          name = "demo-workspace"
 
      }
    }
 
   required_version = ">= 1.2.0"
+  
 }
 
 provider "aws" {
   region = "eu-north-1"
-  # access_key = local.creds.ACCESS_KEY_ID
-  # secret_key = local.creds.SECRET.ACCESS.KEY
-
+  
 }
 
-data "aws_secretsmanager_secret_version" "creds" {
-  secret_id = var.secret_name
-}
 
-locals {
-  creds = jsondecode(
-    data.aws_secretsmanager_secret_version.creds.secret_string
-  )
-}
+
 
