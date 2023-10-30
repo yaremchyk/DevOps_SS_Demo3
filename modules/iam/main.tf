18d5cc3aeec3da8f8209112424e5238e41de5a4a
Ruslan <yarema2003r@gmail.com>
2023-10-30 02:50:41 +0200

    iam edited

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_instance_role" {
-  name               = "${var.namespace}-ec2-intsance-role-${var.environment}"
+  name               = "${var.namespace}_EC2_InstanceRole_${var.environment}"
   assume_role_policy = data.aws_iam_policy_document.ec2_instance_role_policy.json
 }
 
